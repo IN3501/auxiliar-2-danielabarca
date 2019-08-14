32e626b1249e23d7fab0d716d1e2a0639386d245
danielabarca <danielabarcamoya@gmail.com>
2019-08-13 21:48:11 -0400

    desafío 1

---
+++ auxiliar2/miapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'miapp/index.html')
+
+def pestaña(request):
+	return render(request, 'miapp/pestaña.html')
+
+def zapallo(request):
+	return render(request, 'miapp/zapallo.html')
